854733689f0ee355c24260372bcf5388a48e8173
Paul VandenBroeck <PVB96P@gmail.com>
2021-04-06 09:56:40 -0400

    Added Pet Model

---
+++ API/Capstone/Models/Pet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string PetName { get; set; }
+        public DateTime Birthday { get; set; }
+        public char Sex { get; set; }
+        public PetType PetType { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Bio { get; set; }
+
+    }
+
+    public enum PetType
+    {
+        Dog = 1,
+        Cat = 2
+    }
+}
