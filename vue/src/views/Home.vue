d4b2aa09b0ad8de6e322021dc7f0a71ae26d8735
Brandon Oleksik <brandonoleksik@gmail.com>
2021-04-06 12:21:07 -0400

    updated play dates search options. added button to go to play dates page. did limited css styling

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
 <div>
-  Home Page
+  <h1>
+  Pet Play Pals! 
+  </h1>
+<h2>
+Finding your bestfriend's bestfriend
+ </h2>
+ <p>
+  short descriptions
+ </p>
+<router-link to="/playdates" tag="button">Search for PlayDates in your Area!</router-link>
 </div>
+
 </template>
 
 <script>
--- vue/src/views/Playdates.vue
+++ vue/src/views/Playdates.vue
@@ -1,65 +1,101 @@
 <template>
   <div class="playdates">
-      <h1>Play Dates</h1>
-      <table class="form">
-          <tr>
-              <td>Pet Type:</td>
-              <td>
-                  <input type="text" v-model="searchPetType" placeholder="Pet Type"/>
-              </td>
-              <td>Personality:</td>
-              <td>
-                  <input type="text" v-model="searchPersonality" placeholder="Personality"/>
-              </td>
-              <td>
-                  <button type="button" @click="getData">Search</button>
-              </td>
-          </tr>
-      </table>
-<!-- Shows below the list of playdates , TODO need to change to show on a search page-->
-      <playdate-list :playdates="playdates"></playdate-list>
+    <h1>Play Dates</h1>
+    <table class="form">
+      <tr>
+        <td>Pet Type:</td>
+        <td class="dropdown">
+          <select name="searchPetType" id="petType" class="dropdown-content">
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+          </select>
+        </td>
+
+        <td>Personality:</td>
+        
+        <td class="dropdown">
+          <select
+            name="searchPersonality"
+            id="personality"
+            class="dropdown-content"
+          >
+            <option value=""></option>
+            <option value="friendly">Friendly</option>
+            <option value="aggressive">Aggressive</option>
+            <option value="shy">Shy</option>
+            <option value="skittish">Skittish</option>
+            <option value="highEnergy">High-energy</option>
+          </select>
+        </td>
+        <td>Location:</td>
+        <td>
+          <input type="text" v-model="location" placeholder="zip code" class="dropdown-content"/>
+        </td>
+        <td>
+          <button type="button" @click="getData">Search</button>
+        </td>
+      </tr>
+    </table>
+    <!-- Shows below the list of playdates , TODO need to change to show on a search page-->
+    <playdate-list :playdates="playdates"></playdate-list>
   </div>
 </template>
 
 <script>
-import PlaydateList from '@/components/PlaydateList.vue'
-import api from '../services/AuthService.js'
+import PlaydateList from "@/components/PlaydateList.vue";
+import api from "../services/AuthService.js";
 
 export default {
-    name: "playdates",
-    components: {
-        "playdate-list": PlaydateList
-    },
-data(){
+  name: "playdates",
+  components: {
+    "playdate-list": PlaydateList,
+  },
+  data() {
     return {
-        searchPetType: "",
-        searchPersonality: "",
-        playdates: []
+      searchPetType: "",
+      searchPersonality: "",
+      playdates: [],
+      location: "",
     };
-},
-methods :{
-    getData(){
-        // TODO 01: use a service to get data from the server populate the cities array
-        // TODO 02: add in the query string parameters
-        api.getPlaydates(this.searchPetType, this.searchPersonality)
-        .then(
-            (resp) =>{
-                this.playdates=resp.data;
-            }
-        )
-    }
-},
-created(){
+  },
+  methods: {
+    getData() {
+      // TODO 01: use a service to get data from the server populate the cities array
+      // TODO 02: add in the query string parameters
+      api
+        .getPlaydates(this.searchPetType, this.searchPersonality)
+        .then((resp) => {
+          this.playdates = resp.data;
+        });
+    },
+  },
+  created() {
     this.searchPetType = this.$route.query.petType;
     this.searchPersonality = this.$route.query.personality;
     // If PetType is true then get data
-    if (this.searchPetType){
-        this.getData();
+    if (this.searchPetType) {
+      this.getData();
     }
-}
+  },
 };
 </script>
 
 <style scoped>
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.dropdown-content {
+  position: relative;
+  background-color: #d7dff8;
+  color:darkslateblue;
+
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
 
 </style>
