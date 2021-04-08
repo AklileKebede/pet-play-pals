dbc965951d215a4c7b48db1e15095fb7eda8e717
Aklile Kebede <aklile123@gmail.com>
2021-04-08 16:11:41 -0400

    PetForm component- in progress

--- vue/src/components/PetDetails.vue
+++ vue/src/components/PetDetails.vue
@@ -1,32 +1,32 @@
 <template>
-	<div class="shit">
-		<ul>
-			<li>Name: {{ pet.petName }}</li>
-			<li>Bio: {{ pet.bio }}</li>
-			<li>Pet Type: {{ pet.petType }}</li>
-			<li>Breed: {{ pet.breed }}</li>
-			<li>Color: {{ pet.color }}</li>
-			<li>Personality: {{ pet.personality }}</li>
-			<li>Sex: {{ pet.sex }}</li>
-		</ul>
-
-	</div>
+  <div class="petDetails">
+    <ul>
+      <li>Name: {{ pet.petName }}</li>
+      <li>Bio: {{ pet.bio }}</li>
+      <li>Pet Type: {{ pet.petType }}</li>
+      <li>Sex: {{ pet.sex }}</li>
+      <li>Breed: {{ pet.breed }}</li>
+      <li>Color: {{ pet.color }}</li>
+      <li>Personality: {{ pet.personality }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {};
-	},
-	props: {
-		pet: Object,
-	},
+  data() {
+    return {};
+  },
+  props: {
+    pet: Object,
+  },
 };
 </script>
 
 <style scoped>
-.shit {
-	background-color: rgb(80, 66, 19);
-	color: burlywood;
+.petDetails {
+  background-color: rgb(80, 66, 19);
+  color: burlywood;
 }
+
 </style>
---
+++ vue/src/components/PetForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form id="PetForm">
+      <h1>Example h1: Register Your Pet</h1>
+      <p> Pet Name: 
+        <input type="text" v-model="location" placeholder="Pet Name" />
+      </p>
+      <li>Bio: <input type="text" v-model="location" placeholder="Bio" /></li>
+      <li>Pet Type: <input type="text" v-model="location" placeholder="Pet Type" /></li>
+      <li>Sex: <input type="text" v-model="location" placeholder="Sex" /></li>
+      <li>Breed: <input type="text" v-model="location" placeholder="Breed" /></li>
+      <li>Color: <input type="text" v-model="location" placeholder="Color" /></li>
+      <li>Personality: <input type="text" v-model="location" placeholder="Personality" /></li>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "PetForm",
+    props: {},
+    data(){
+        return{
+            pet: {
+                petId:0,
+                petName: "",
+                bio:"",
+                petType:"",
+                sex:"",
+                breed:"",
+                color:"",
+                personality:""
+            }
+        };
+    },
+    methods:{
+        addPet(){
+            // use the service to Add the pet on the server (Post)
+            
+        }
+    },
+};
+</script>
+
+<style>
+</style>
