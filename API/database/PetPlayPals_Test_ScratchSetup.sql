9f1e0a77eab0b455dcb7b7288f82da08cb6fe6bc
Brandon Oleksik <brandonoleksik@gmail.com>
2021-04-08 15:11:41 -0400

    updated database to reflect multiple pets for brandon

--- API/database/PetPlayPals_Test_ScratchSetup.sql
+++ API/database/PetPlayPals_Test_ScratchSetup.sql
@@ -24,7 +24,8 @@ insert into pets(pet_name, birthday, sex, pet_type_id, pet_breed, color, bio) va
 insert into pets(pet_name, birthday, sex, pet_type_id, pet_breed, color, bio) values ('Maggie', '05-12-2019', 'F', 1, 'Goldendoodle', 'Golden', 'Perfect snuggler'  );
 insert into pets(pet_name, birthday, sex, pet_type_id, pet_breed, color, bio) values ('Carl', '01-28-2020', 'M', 1, 'Mix', 'Brown and Black', 'Loud boy');
 insert into pets(pet_name, birthday, sex, pet_type_id, pet_breed, color, bio) values ('Hanzo', '01-15-2015', 'M', 2, 'Brown Tabby', 'Brown and Black', 'HAMzo is fat');
-
+insert into pets(pet_name, birthday, sex, pet_type_id, pet_breed, color, bio) values ('Pippin', '01-15-2020', 'M', 2, 'American Shorthair', 'Grey and White', 'pippin is dumb');
+insert into pets(pet_name, birthday, sex, pet_type_id, pet_breed, color, bio) values ('Teddy', '01-15-2011', 'M', 1, 'Pitbull', 'Golden', 'Teddy is cuddly');
 
 select * from pets
 
@@ -32,7 +33,7 @@ select * from pets
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('brandon', 'V0lRjxFQxgKeP+/h5IbKqnAPQoU=', 'Rz+Z8yMoqIg=', 'user');
 insert into users (username, password_hash, salt, user_role) values ('rosa', 'EBbMAfKO8NXslvRz6GCCVmeV6ig=', 'ybDc/+RXVqs=', 'user');
 insert into users (username, password_hash, salt, user_role) values ('paul', 'fp9wBngxMgdo2HIcts7YLBJdhyU=', 'Sqzc1pc53es=', 'user');
-insert into users (username, password_hash, salt, user_role)values ('aklile', 'HqkX65RZf4sM+SaOwjcVhDKJgvY=', 'v7N29suiPBA=', 'user');
+insert into users (username, password_hash, salt, user_role) values ('aklile', 'HqkX65RZf4sM+SaOwjcVhDKJgvY=', 'v7N29suiPBA=', 'user');
 
 select * from users
 
@@ -41,6 +42,8 @@ insert into user_pet(user_id, pet_id) Values(3,4);
 insert into user_pet(user_id, pet_id) Values(4,1);
 insert into user_pet(user_id, pet_id) Values(5,2);
 insert into user_pet(user_id, pet_id) Values(6,3);
+insert into user_pet(user_id, pet_id) Values(3,5);
+insert into user_pet(user_id, pet_id) Values(3,6);
 
 select * from user_pet 
 
