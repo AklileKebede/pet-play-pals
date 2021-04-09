b777c7a23a16e12e70dc52db9ec2a1c850e356f7
Paul VandenBroeck <PVB96P@gmail.com>
2021-04-09 10:40:03 -0400

    Meaningless change to the scratrch setup

--- API/database/PetPlayPals_Test_ScratchSetup.sql
+++ API/database/PetPlayPals_Test_ScratchSetup.sql
@@ -63,7 +63,7 @@ insert into personality_pet (personality_id, pet_id) values (1, 4);
 insert into personality_pet (personality_id, pet_id) values (1, 2);
 insert into personality_pet (personality_id, pet_id) values (7, 2);
 
-select personality_name from personality join personality_pet on personality.personality_id = personality_pet.personality_id where personality_pet.pet_id = 1
+select personality_name,personality.personality_id from personality join personality_pet on personality.personality_id = personality_pet.personality_id where personality_pet.pet_id = 2
 select personality.personality_id from personality join personality_pet on personality.personality_id = personality_pet.personality_id where personality_pet.pet_id = 1
 
 select * from pet_types
