71b9ae3c6f6696da83bc36a2b0f23591a6da9997
Rosa Buss <marissa.buss01@gmail.com>
2021-04-11 16:56:48 -0400

    playdate details component

---
+++ vue/src/components/PlaydateDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <li> Playdate Id: {{playdate.playdateId}}</li>
+      <li> Date: {{playdate.date}}</li>
+      <li> Playdate Organizer: {{playdate.userId}}</li>
+      <li>Participating Pets: 
+         <ul>
+          <li v-for="pet in playdate.pets" v-bind:key = "pet">{{pet.petName}}|{{pet.petType}}</li>
+        </ul>
+      </li>
+      <li>Location: {{playdate.location}}</li>
+      <li></li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+  return {};
+},
+name: "playdate-detail",
+props: {
+    playdates: Object,
+},
+}
+</script>
+
+<style scoped>
+
+</style>
