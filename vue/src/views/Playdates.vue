2431400aff703277f6000ce293b028d6ea58676c
Aklile Kebede <aklile123@gmail.com>
2021-04-12 11:15:34 -0400

    Playdates Search updated, playdates map component in progress

--- vue/src/views/Playdates.vue
+++ vue/src/views/Playdates.vue
@@ -1,132 +1,149 @@
 <template>
-	<div class="playdates">
-		<h1>Play Dates Search</h1>
-		<table class="form">
-			<tr></tr>
-			<tr>
-				Pet Type:
-			</tr>
-			<td class="dropdown">
-				<select
-					name="searchPetType"
-					id="petType"
-					class="dropdown-content"
-				>
-					<option value=""></option>
-					<option value="dog">Dog</option>
-					<option value="cat">Cat</option>
-				</select>
-			</td>
+  <div class="playdates">
+    <div id="searchFunction">
+      <h1>Play Dates Search</h1>
+      <table class="form">
+        <tr></tr>
+        <tr>
+          Pet Type:
+        </tr>
+        <td class="dropdown">
+          <select
+            name="searchPetType"
+            id="petType"
+            class="dropdown-content"
+            v-model="filterParams.petTypeId"
+          >
+            <option value=""></option>
+            <option
+              v-for="(item, key) in $store.state.validPetTypes"
+              v-bind:key="key"
+              v-bind:value="key"
+            >
+              {{ item }}
+            </option>
+          </select>
+        </td>
 
-			<tr>
-				Personality:
-			</tr>
+        <tr>
+          Personality:
+        </tr>
 
-			<td class="dropdown">
-				<select
-					name="searchPersonality"
-					id="personality"
-					class="dropdown-content"
-				>
-					<option value=""></option>
-					<option value="friendly">Friendly</option>
-					<option value="Plays Rough">Plays Rough</option>
-					<option value="shy">Shy</option>
-					<option value="skittish">Skittish</option>
-					<option value="highEnergy">High-energy</option>
-					<option value="Reactive">Reactive</option>
-					<option value="Gentle">Gentle</option>
-				</select>
-			</td>
-			<tr>
-				Location:
-			</tr>
-			<td>
-				<input
-					type="text"
-					v-model="location"
-					placeholder="zip code"
-					class="dropdown-content"
-				/>
-			</td>
-			<tr>
-				<button type="button" @click="getData" class="green-button">
-					Search
-				</button>
-			</tr>
-		</table>
-		<h2>Search Results</h2>
-		<!-- Shows below the list of playdates , TODO need to change to show on a search page-->
-	</div>
+        <td class="dropdown">
+          <!-- <select
+            name="searchPersonality"
+            id="personality"
+            class="dropdown-content"
+             v-model="filterParams.personalityIds"
+          > -->
+          <ul>
+            <li
+              v-for="(item, key) in $store.state.validPersonalities"
+              v-bind:key="key"
+            >
+              {{ item }}
+              <input
+                type="checkbox"
+                v-bind:value="key"
+                v-model="filterParams.personalityIds"
+              />
+            </li>
+          </ul>
+        </td>
+        <tr>
+          Location:
+        </tr>
+        <td>
+          <input
+            type="text"
+            v-model="filterParams.location"
+            placeholder="zip code"
+            class="dropdown-content"
+          />
+        </td>
+        <tr>
+          <button type="button" @click="getData" class="green-button">
+            Search
+          </button>
+        </tr>
+      </table>
+    </div>
+    <div id=searchMap>
+      <playdate-map></playdate-map>
+    </div>
+    <div id="searchResults">
+      <h2>Search Results</h2>
+      <!-- Shows below the list of playdates , TODO need to change to show on a search page-->
+    </div>
+  </div>
 </template>
 
 <script>
-import api from "@/services/AuthService.js";
-import PlaydatesService from "@/services/PlaydatesService"
+// import api from "@/services/AuthService.js";
+import PlaydatesService from "@/services/PlaydatesService";
 import "@/cssStyles/style.css";
 
 export default {
-	name: "playdates",
-	components: {},
-	data() {
-		return {
-			searchPetType: "",
-			searchPersonality: "",
-			location: "",
-			playdates: {},
-
-		};
-	},
-	methods: {
-		getData() {
-			// TODO 02: add in the query string parameters
-			api.getPlaydates(this.searchPetType, this.searchPersonality).then(
-				(resp) => {
-					this.playdates = resp.data;
-				}
-			);
-		},
-		search() {
-			PlaydatesService.getPlaydates().then((response) => {
-				this.playdates = response.data;
-			})
-		}
-	},
-	created() {
-		this.searchPetType = this.$route.query.petType;
-		this.searchPersonality = this.$route.query.personality;
-		// If PetType is true then get data
-		if (this.searchPetType) {
-			this.getData();
-		}
-	},
+  name: "playdates",
+  components: {},
+  data() {
+    return {
+      // searchPetType: "",
+      // searchPersonality: "",
+      // location: "",
+      playdates: {},
+      filterParams: { personalityIds: [] },
+    };
+  },
+  methods: {
+    getData() {
+      // TODO 02: add in the query string parameters
+      PlaydatesService.getPlaydates(this.filterParams).then((resp) => {
+        this.playdates = resp.data;
+      });
+    },
+    // search() {
+    //   PlaydatesService.getPlaydates().then((response) => {
+    //     this.playdates = response.data;
+    //   });
+    // },
+  },
+  created() {
+    // this.searchPetType = this.$route.query.petType;
+    // this.searchPersonality = this.$route.query.personality;
+    // If PetType is true then get data
+    // if (this.searchPetType) {
+      this.getData();
+      // this.search();
+    // }
+  },
 };
 </script>
 
 <style>
 .playdates {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .dropdown:hover .dropdown-content {
-	display: block;
+  display: block;
 }
 .dropdown {
-	position: relative;
-	display: inline-block;
+  position: relative;
+  display: inline-block;
 }
 .dropdown-content {
-	position: relative;
-	background-color: whitesmoke;
-	color: #0d7685;
+  position: relative;
+  background-color: whitesmoke;
+  color: #0d7685;
 
-	min-width: 160px;
-	box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-	z-index: 1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
 }
 h1 {
-	text-align: center;
+  text-align: center;
 }
 </style>
