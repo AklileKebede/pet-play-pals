8311ec2d6565e270eafd4536769fc2bdbf89b90a
Rosa Buss <marissa.buss01@gmail.com>
2021-04-12 11:19:29 -0400

    editable username backend

---
+++ API/Capstone/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : AuthorizedControllerBase
+    {
+        private readonly IUserDAO userDAO;
+
+        public UsersController(IUserDAO userDAO)
+        {
+            this.userDAO = userDAO;
+        }
+
+        [HttpPost("{id}")]
+        public ActionResult<User> UpdateUsername(int userId, string username)
+        {
+            User updatedUser = null;
+
+            if (userDAO.UpdateUsername(username, userId) == 1)
+            {
+                updatedUser = userDAO.GetUserById(userId);
+            }
+
+            return updatedUser;
+
+        }
+
+    }
+}
--- API/Capstone/DAO/UserSqlDAO.cs
+++ API/Capstone/DAO/UserSqlDAO.cs
@@ -16,6 +16,60 @@ namespace Capstone.DAO
             connectionString = dbConnectionString;
         }
 
+        public int UpdateUsername(string username, int userId)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SqlConnection conn =  new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("update users set username = @username where user_id = @userId");
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@user_id", userId);
+                    rowsAffected = cmd.ExecuteNonQuery();
+
+                }
+
+            }
+            catch(SqlException)
+            {
+                throw;
+            }
+
+            return rowsAffected;
+        }
+
+        public User GetUserById(int userId)
+        {
+            User returnUser = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT user_id, username, password_hash, salt, user_role FROM users WHERE user_id = @userId", conn);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows && reader.Read())
+                    {
+                        returnUser = GetUserFromReader(reader);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return returnUser;
+
+        }
+
         public User GetUser(string username)
         {
             User returnUser = null;
