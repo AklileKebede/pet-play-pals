a5072aabd26860f8b673a9784047b2cb981dbabc
Aklile Kebede <aklile123@gmail.com>
2021-04-14 13:11:33 -0400

    in progress possible conflict

--- vue/src/views/CreatePlaydate.vue
+++ vue/src/views/CreatePlaydate.vue
@@ -1,13 +1,75 @@
 <template>
-		<div>
-		<form id="creatPlaydate" v-on:submit.prevent="submitForm">
-			<h1>Create New Playdate</h1>
-			<ul>
-				<li>
-					<!-- Need to adjust so the playdate connects petId -->
-					<label for="petName">Playdate created for:{{Playdate.petName}}</label>
-				</li>
-				<li>
+  <div>
+    <form id="creatPlaydate" v-on:submit.prevent="submitForm">
+      <h1>Create New Playdate</h1>
+
+      <p>create playdate</p>
+      <!--      <tr>
+          Pet Type:
+        </tr>
+        <td class="dropdown">
+          <ul>
+            <li
+              v-for="(item, key) in $store.state.validPetTypes"
+              v-bind:key="key"
+              v-bind:value="key"
+            >
+              {{ item }}
+              <input
+                type="checkbox"
+                v-bind:value="key"
+                v-model="filterParams.petTypeIds"
+              />
+            </li>
+          </ul>
+        </td> -->
+      <li>
+        Pet Type Allowed:
+        <select
+          name="petType"
+          id="petType"
+          class="dropdown-content"
+          v-model="playdate.petTypeId"
+        >
+          <option value=""></option>
+          <option
+            v-for="(item, key) in $store.state.validPetTypes"
+            v-bind:key="key"
+            v-bind:value="key"
+          >
+            {{ item }}
+          </option>
+        </select>
+      </li>
+      <li>
+        Personality:
+        <ul>
+          <li
+            v-for="(item, key) in $store.state.validPersonalities"
+            v-bind:key="key"
+          >
+            <label v-bind:for="key">{{ item }}</label>
+            <input
+              v-bind:id="key"
+              type="checkbox"
+              v-bind:value="key"
+              v-model="playdate.personalityIds"
+            />
+          </li>
+        </ul>
+      </li>
+      <button type="submit" class="smallGreenButton">Submit</button>
+    </form>
+    <router-link
+      to="/home/profile"
+      tag="button"
+      id="creatPlaydate"
+      class="smallGreenButton"
+      >Cancel</router-link
+    >
+  </div>
+
+  <!-- <li>
 					<label for="description">Playdate description:</label>
 				</li>
 				<li>
@@ -20,18 +82,7 @@
 				</li>
 			</ul>
 
-			<li>
-				Pet Type Allowed:
-				<select
-					name="petType"
-					id="petType"
-					class="dropdown-content"
-					v-model="playdate.petTypeId"
-				>
-					<option value=""></option>
-					<option v-for="(item, key) in $store.state.validPetTypes" v-bind:key="key" v-bind:value="key">{{item}}</option>
-				</select>
-			</li>
+			
 			<li>
 				<label for="startDate">Start Time:</label>
 
@@ -86,35 +137,10 @@
 					placeholder="Color"
 				/>
 			</li>
-			<li>
-				Personality:
-				<ul>
-					<li
-						v-for="(item, key) in $store.state.validPersonalities"
-						v-bind:key="key"
-					>
-						<label v-bind:for="key">{{ item }}</label>
-						<input
-							v-bind:id="key"
-							type="checkbox"
-							v-bind:value="key"
-							v-model="pet.personalityIds"
-						/>
-					</li>
-				</ul>
-			</li>
-			<button type="submit" class="smallGreenButton">Submit</button>
+	
 		</form>
-
-		<router-link
-			to="/home/profile"
-			tag="button"
-			id="petForm"
-			class="smallGreenButton"
-			>Cancel</router-link
-		>
-	</div>
-		<!-- make me a new playdate
+	</div> -->
+  <!-- make me a new playdate
 	location
 	map
 	playdate organizer
@@ -126,44 +152,40 @@
 </template>
 
 <script>
-import PlaydatesService from "../services/PlaydatesService.js"
+import PlaydatesService from "../services/PlaydatesService.js";
 export default {
-	name: "createPlaydate",
-	components:{},
-	data(){
-		return{
-			playdate:{},
-			allPersonalities:{},
-			
-		};
-	},
-	computed: {
-		currentUser() {
-			return this.$store.state.user;
-		},
-	},
-	methods:{
-		submitForm(){
-			PlaydatesService.addNewPlaydate(this.playdate)
-			.then(()=>{
-				this.$router.push({name:""})
-			});
-		},
-	},
-	created(){
-
-	},
+  name: "createPlaydate",
+  components: {},
+  data() {
+    return {
+      playdate: {},
+      allPersonalities: {},
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.user;
+    },
+  },
+  methods: {
+    submitForm() {
+      PlaydatesService.addNewPlaydate(this.playdate).then(() => {
+        this.$router.push({ name: "" });
+      });
+    },
+  },
+  created() {},
 };
 </script>
 
 <style scoped>
-input:invalid+span:after {
-    content: '✖';
-    padding-left: 5px;
+input:invalid + span:after {
+  content: "✖";
+  padding-left: 5px;
 }
 
-input:valid+span:after {
-    content: '✓';
-    padding-left: 5px;
+input:valid + span:after {
+  content: "✓";
+  padding-left: 5px;
 }
 </style>
