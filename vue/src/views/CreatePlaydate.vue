e82ef8790fa945d531500be414a467b40e64b58f
Aklile Kebede <aklile123@gmail.com>
2021-04-14 14:49:50 -0400

    Create Playdate- Frontend in progress

--- vue/src/views/CreatePlaydate.vue
+++ vue/src/views/CreatePlaydate.vue
@@ -2,44 +2,22 @@
   <div>
     <form id="creatPlaydate" v-on:submit.prevent="submitForm">
       <h1>Create New Playdate</h1>
-
-      <p>create playdate</p>
-      <!--      <tr>
-          Pet Type:
-        </tr>
-        <td class="dropdown">
-          <ul>
-            <li
-              v-for="(item, key) in $store.state.validPetTypes"
-              v-bind:key="key"
-              v-bind:value="key"
-            >
-              {{ item }}
-              <input
-                type="checkbox"
-                v-bind:value="key"
-                v-model="filterParams.petTypeIds"
-              />
-            </li>
-          </ul>
-        </td> -->
       <li>
         Pet Type Allowed:
-        <select
-          name="petType"
-          id="petType"
-          class="dropdown-content"
-          v-model="playdate.petTypeId"
-        >
-          <option value=""></option>
-          <option
+        <ul>
+          <li
             v-for="(item, key) in $store.state.validPetTypes"
             v-bind:key="key"
             v-bind:value="key"
           >
             {{ item }}
-          </option>
-        </select>
+            <select v-model="playdate.petTypesPermitted[key]">
+              <option value="True">Allowed</option>
+              <option value="null">-</option>
+              <option value="False">Disallowed</option>
+            </select>
+          </li>
+        </ul>
       </li>
       <li>
         Personality:
@@ -48,15 +26,18 @@
             v-for="(item, key) in $store.state.validPersonalities"
             v-bind:key="key"
           >
-            <label v-bind:for="key">{{ item }}</label>
-            <input
-              v-bind:id="key"
-              type="checkbox"
-              v-bind:value="key"
-              v-model="playdate.personalityIds"
-            />
+            {{ item }}
+            <select v-model="playdate.personalitiesPermitted[key]">
+              <option value="True">Allowed</option>
+              <option value="null">-</option>
+              <option value="False">Disallowed</option>
+            </select>
           </li>
         </ul>
+      </li>
+      <li>
+        Location:
+        <map-searchbox></map-searchbox>
       </li>
       <button type="submit" class="smallGreenButton">Submit</button>
     </form>
@@ -67,79 +48,12 @@
       class="smallGreenButton"
       >Cancel</router-link
     >
+    <div id="searchMap" class="bubble foggy-gray-bg">
+      <!-- <playdate-map></playdate-map> -->
+      <div class="map" id="map" />
+    </div>
   </div>
 
-  <!-- <li>
-					<label for="description">Playdate description:</label>
-				</li>
-				<li>
-					<textarea
-						id="description"
-						v-model="Playdate.description"
-						placeholder="Playdate Description"
-						style="width: -webkit-fill-available"
-					></textarea>
-				</li>
-			</ul>
-
-			
-			<li>
-				<label for="startDate">Start Time:</label>
-
-				<input
-					type="datetime-local"
-					id="startDate"
-					min="today"
-					v-model="playdate.startDate"
-					placeholder="today"
-				/>
-				<span class="validity"></span>
-			</li>
-			<li>
-				<label for="endDate">End Time:</label>
-
-				<input
-					type="datetime-local"
-					id="endDate"
-					v-model="playdate.endDate"
-					placeholder="today"
-				/>
-			</li>
-
-			<li>
-				<label for="petSex">Pet Sex</label>
-				<select
-					name="petSex"
-					id="petSex"
-					v-model="pet.sex"
-					class="dropdown-content"
-				>
-					<option value=""></option>
-					<option value="F">Female</option>
-					<option value="M">Male</option>
-				</select>
-			</li>
-			<li>
-				<label for="breed">Breed: </label>
-				<input
-					type="text"
-					id="breed"
-					v-model="pet.breed"
-					placeholder="Breed"
-				/>
-			</li>
-			<li>
-				<label for="color">Color: </label>
-				<input
-					type="text"
-					id="color"
-					v-model="pet.color"
-					placeholder="Color"
-				/>
-			</li>
-	
-		</form>
-	</div> -->
   <!-- make me a new playdate
 	location
 	map
@@ -153,13 +67,13 @@
 
 <script>
 import PlaydatesService from "../services/PlaydatesService.js";
+import MapSearchbox from "../components/MapSearchBox.vue";
 export default {
   name: "createPlaydate",
-  components: {},
+  components: { MapSearchbox },
   data() {
     return {
-      playdate: {},
-      allPersonalities: {},
+      playdate: { petTypesPermitted: {}, personalitiesPermitted: {} },
     };
   },
   computed: {
