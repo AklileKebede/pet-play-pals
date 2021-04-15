0e9d3ef405d375c84e3016b518314acb9888abd7
Paul VandenBroeck <PVB96P@gmail.com>
2021-04-15 14:43:54 -0400

    TEMPORARY NETWORK ERROR FIX: Finally we dont have to live with the constant "network error" bugs we've been getting. I still blame axios. anyway, the fix is when we get one of these silly errors, just refresh the page. and keep refreshing until wee get zero errors. its pretty hacky, but i just want the app to work

--- vue/src/services/AuthService.js
+++ vue/src/services/AuthService.js
@@ -1,17 +1,51 @@
 import axios from 'axios';
-import axiosRetry from 'axios-retry'
 
 
 
 const http = axios.create(
 	{// Base URL of every endpoint that we will be calling
-		baseURL: process.env.VUE_APP_REMOTE_API
+		baseURL: process.env.VUE_APP_REMOTE_API,
+
 	});
 
-axiosRetry(http, {
-	retries: 15,
-	retryCondition: true
+// // Add a request interceptor
+// axios.interceptors.request.use(function (config) {
+// 	// Do something before request is sent
+// 	return config;
+// }, function (error) {
+// 	// Do something with request error
+// 	console.log("request error");
+// 	return Promise.reject(error);
+// });
+
+// // Add a response interceptor
+// axios.interceptors.response.use(function (response) {
+// 	// Do something with response data
+// 	console.log("response error");
+// 	return response;
+// }, function (error) {
+// 	// Do something with response error
+// 	return Promise.reject(error);
+// });
+
+//add interceptor
+axios.interceptors.response.use((response) => response, (error) => {
+
+	if (!error.response) {
+		//todo: this is super hacky but maybe works
+		//it just keeps reloading the page until it works!
+		location.reload(true);
+
+	} else {
+		console.error(error)
+	}
 });
+
+axios.interceptors.request.use((request) => request, (error) => {
+	console.log("send error");
+	console.error(error)
+})
+
 export default {
 
 	login(user) {
