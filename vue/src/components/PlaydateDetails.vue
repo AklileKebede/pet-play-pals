fe5b22d0c4ec46e8bd7090187a20ec9b2d4ce2a3
Paul VandenBroeck <PVB96P@gmail.com>
2021-04-15 09:48:36 -0400

    Almost got the ability to add/remove your pets from a playdate

--- vue/src/components/PlaydateDetails.vue
+++ vue/src/components/PlaydateDetails.vue
@@ -1,36 +1,52 @@
 <template>
-	<div>
-		<ul>
-			<li>Playdate Id: {{ playdate.playdateId }}</li>
-			<li>Playdate Start Time:{{ playdate.startDateTime }}</li>
-			<li>Playdate End Time: {{ playdate.endDateTime }}</li>
-			<!-- <li>Location: {{ playdate.location.address }}</li> -->
-			<li>Playdate Discription:{{ playdate.description }}</li>
-			<li>Playdate Organizer: {{ playdate.userName }}</li>
-			<li>
-				Participating Pets:
-				<ul>
-					<li v-for="pet in playdate.participants" v-bind:key="pet">
-						<pet-details v-bind:pet="pet"></pet-details>
-					</li>
-				</ul>
-			</li>
-			<li>
-				<h2>Add your pets to this playdate</h2>
-				<div v-for="myPet in myPets" v-bind:key="myPet">
-					<pet-details v-bind:pet="myPet"></pet-details>
-					<button class="smallGreenButton" v-if="!playdate.participants.find((p)=>{p.petId === myPet.petId})" v-on:click="addPet(myPet)">Add pet</button>
-					<button class="smallGreenButton" v-if="playdate.participants.find((p)=>{p.petId === myPet.petId})" v-on:click="removePet(myPet)">remove pet</button>
-				</div>
-			</li>
-		</ul>
+	<div id="petDetailsOuterDiv">
+		<div id="petDetails" class="bubble foggy-gray-bg">
+			<ul>
+				<li>Playdate Id: {{ playdate.playdateId }}</li>
+				<li>Playdate Start Time:{{ playdate.startDateTime }}</li>
+				<li>Playdate End Time: {{ playdate.endDateTime }}</li>
+				<!-- <li>Location: {{ playdate.location.address }}</li> -->
+				<li>Playdate Discription:{{ playdate.description }}</li>
+				<li>Playdate Organizer: {{ playdate.userName }}</li>
+				<li>
+					Participating Pets:
+					<ul>
+						<li
+							v-for="pet in playdate.participants"
+							v-bind:key="pet.petId"
+						>
+							<pet-details v-bind:pet="pet"></pet-details>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div id="addRemovePets" class="bubble foggy-gray-bg">
+			<ul>
+				<li>
+					<h2>Add your pets to this playdate</h2>
+					<div v-for="myPet in myPets" v-bind:key="myPet.petId">
+						<pet-details v-bind:pet="myPet"></pet-details>
+						<label v-bind:for="`addRemove${myPet.petId}`"
+							>add pet to/remove pet from</label
+						>
+						<input
+							type="checkbox"
+							v-bind:value="myPet"
+							v-model="playdate.participants"
+							v-bind:id="`addRemove${myPet.petId}`"
+						/>
+					</div>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
 import PlaydatesService from "../services/PlaydatesService.js";
 import petDetails from "../components/PetDetails";
-import PetService from '../services/PetsService'
+import PetService from "../services/PetsService";
 
 export default {
 	name: "playdate-details",
@@ -64,17 +80,29 @@ export default {
 				}
 			);
 		},
-		addPet(petToAdd){
+		addPet(petToAdd) {
 			this.playdate.participants.push(petToAdd);
 		},
-		removePet(petToRemove){
+		removePet(petToRemove) {
 			this.playdate.participants.delete(petToRemove);
 		},
-	},created(){
+	},
+	created() {
 		this.getPets();
-	}
+	},
 };
 </script>
 
 <style>
+#playdateDetails {
+	grid-area: "playdateDetails";
+}
+#addRemovePets {
+	grid-area: "addRemovePets";
+}
+#petDetailsOuterDiv {
+	display: grid;
+	grid-gap: 10px;
+	grid-template-areas: "addRemovePets playdateDetails";
+}
 </style>
