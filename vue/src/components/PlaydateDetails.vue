168ca4ad115fb15b563c77ef786ff6961c5b095e
Paul VandenBroeck <PVB96P@gmail.com>
2021-04-15 09:48:42 -0400

    Almost got the ability to add/remove your pets from a playdate

--- vue/src/components/PlaydateDetails.vue
+++ vue/src/components/PlaydateDetails.vue
@@ -1,44 +1,35 @@
 <template>
 	<div id="petDetailsOuterDiv">
 		<div id="petDetails" class="bubble foggy-gray-bg">
+			<h2>Playdate Details</h2>
 			<ul>
 				<li>Playdate Id: {{ playdate.playdateId }}</li>
 				<li>Playdate Start Time:{{ playdate.startDateTime }}</li>
 				<li>Playdate End Time: {{ playdate.endDateTime }}</li>
 				<!-- <li>Location: {{ playdate.location.address }}</li> -->
-				<li>Playdate Discription:{{ playdate.description }}</li>
+				<li>Playdate description:{{ playdate.description }}</li>
 				<li>Playdate Organizer: {{ playdate.userName }}</li>
-				<li>
-					Participating Pets:
-					<ul>
-						<li
-							v-for="pet in playdate.participants"
-							v-bind:key="pet.petId"
-						>
-							<pet-details v-bind:pet="pet"></pet-details>
-						</li>
-					</ul>
+				<li>Participating Pets:</li>
+
+				<li v-for="pet in playdate.participants" v-bind:key="pet.petId">
+					<pet-details v-bind:pet="pet"></pet-details>
 				</li>
 			</ul>
 		</div>
 		<div id="addRemovePets" class="bubble foggy-gray-bg">
-			<ul>
-				<li>
-					<h2>Add your pets to this playdate</h2>
-					<div v-for="myPet in myPets" v-bind:key="myPet.petId">
-						<pet-details v-bind:pet="myPet"></pet-details>
-						<label v-bind:for="`addRemove${myPet.petId}`"
-							>add pet to/remove pet from</label
-						>
-						<input
-							type="checkbox"
-							v-bind:value="myPet"
-							v-model="playdate.participants"
-							v-bind:id="`addRemove${myPet.petId}`"
-						/>
-					</div>
-				</li>
-			</ul>
+			<h2>Add your pets to this playdate</h2>
+			<div v-for="myPet in myPets" v-bind:key="myPet.petId">
+				<pet-details v-bind:pet="myPet"></pet-details>
+				<label v-bind:for="`addRemove${myPet.petId}`"
+					>add pet to/remove pet from</label
+				>
+				<input
+					type="checkbox"
+					v-bind:value="myPet"
+					v-model="playdate.participants"
+					v-bind:id="`addRemove${myPet.petId}`"
+				/>
+			</div>
 		</div>
 	</div>
 </template>
@@ -80,12 +71,6 @@ export default {
 				}
 			);
 		},
-		addPet(petToAdd) {
-			this.playdate.participants.push(petToAdd);
-		},
-		removePet(petToRemove) {
-			this.playdate.participants.delete(petToRemove);
-		},
 	},
 	created() {
 		this.getPets();
@@ -95,14 +80,15 @@ export default {
 
 <style>
 #playdateDetails {
-	grid-area: "playdateDetails";
+	grid-area: playdateDetails;
 }
 #addRemovePets {
-	grid-area: "addRemovePets";
+	grid-area: addRemovePets;
 }
 #petDetailsOuterDiv {
 	display: grid;
 	grid-gap: 10px;
-	grid-template-areas: "addRemovePets playdateDetails";
+	grid-template-columns: 1fr 1fr;
+	grid-template-areas: "playdateDetails addRemovePets";
 }
 </style>
