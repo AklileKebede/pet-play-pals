8dadf2d7f60a89b3b3a6113bae20151a5386af71
Aklile Kebede <aklile123@gmail.com>
2021-04-15 11:49:52 -0400

    profile pet details formating completed

--- vue/src/components/PetDetails.vue
+++ vue/src/components/PetDetails.vue
@@ -1,62 +1,68 @@
 <template>
-
-	<div id="petDetails" class="petDetails bubble foggy-gray-bg">
-
-		<ul>
-			<li>ID: {{ pet.petId }}</li>
-			<li>Name: {{ pet.petName }}</li>
-			<li>Bio: {{ pet.bio }}</li>
-			<li>Pet Type: {{ pet.petType }}</li>
-			<li>Sex: {{ pet.sex }}</li>
-			<li>Breed: {{ pet.breed }}</li>
-			<li>Color: {{ pet.color }}</li>
-			<li>
-				Personality:
-				<ul>
-					<li
-						v-for="personality in pet.personalities"
-						v-bind:key="personality"
-					>
-						{{ personality }}
-					</li>
-				</ul>
-			</li>
-			<!--this is for adding the pet images -->
-				<img
-					class="profilePic bubble"
-					v-bind:src="pet.imgUrl"
-					alt="pet profile pic"
-				/>
-		</ul>
-	</div>
+  <div id="petDetails" class="petDetails bubble foggy-gray-bg">
+    <ul id="petDetailsArea">
+      <li>ID: {{ pet.petId }}</li>
+      <li>Name: {{ pet.petName }}</li>
+      <li>Bio: {{ pet.bio }}</li>
+      <li>Pet Type: {{ pet.petType }}</li>
+      <li>Sex: {{ pet.sex }}</li>
+      <li>Breed: {{ pet.breed }}</li>
+      <li>Color: {{ pet.color }}</li>
+      <li>
+        Personality:
+        <ul>
+          <li v-for="personality in pet.personalities" v-bind:key="personality">
+            {{ personality }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div id="imgArea">
+      <!--this is for adding the pet images -->
+      <img
+        class="profilePic bubble"
+        v-bind:src="pet.imgUrl"
+        alt="pet profile pic"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "pet-details",
-	data() {
-		return {};
-	},
-	props: {
-		pet: Object,
-	},
+  name: "pet-details",
+  data() {
+    return {};
+  },
+  props: {
+    pet: Object,
+  },
 };
 </script>
 
 <style scoped>
-.petDetails{
-    list-style-type: none;
-    padding: 20px;
-    background-color: #0D7685;
-	margin: 10px;
+.imgArea {
+  grid-area: imgArea;
+}
+.petDetailsArea {
+  grid-area: petDetailsArea;
+}
+.petDetails {
+  display: grid;
+  grid-template-areas: "imgArea petDetailsArea";
 }
 
-.profilePic {
-	max-height: 200px;
-	max-width: 300px;
-  align-content: center;
+.petDetails {
+  list-style-type: none;
+  padding: 20px;
+  background-color: #0d7685;
   margin: 10px;
 }
 
-
+.profilePic {
+  max-height: 200px;
+  max-width: 300px;
+  align-content: center;
+  margin: 10px;
+}
 </style>
